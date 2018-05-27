f8d2ef4d1726c6c6df86399bfdceaa2c6730c934
Alyson Maia <alyson.maia@gmail.com>
2018-05-27 04:57:49 -0300

    App component moved to containers/App folder

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CssBaseline />
+        <h1>Play Nine</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
