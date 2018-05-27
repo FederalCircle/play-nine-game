a9d99a9e5fb4031d57513073c8ccadda2795aee7
Alyson Maia <alyson.maia@gmail.com>
2018-05-27 07:47:02 -0300

    [StarList] Component added

---
+++ src/components/StarList/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
+import Card from '../Card';
+import Star from '../Star';
+import NumberChip from '../NumberChip'
+import './StarList.css'
+
+function starList({ quantity }) {
+  return (
+    <div className="StarList">
+      <Card>
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          style={{ height: '140px' }}
+        >
+          {[ ...Array(quantity) ].map((value, id) =>
+            <Grid item key={ id } style={{ width: '20%' }}>
+              <Star />
+            </Grid>
+          )}
+        </Grid>
+      </Card>
+    </div>
+  )
+}
+
+starList.propTypes = {
+  quantity: PropTypes.number
+}
+
+starList.defaultProps = {
+  quantity: 10
+}
+
+export default starList
